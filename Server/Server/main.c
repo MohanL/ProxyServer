f9475572e626f61c0a3afcdbdea3ddcea3a46ddd
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-22 14:57:39 -0400

    simple echo server

--- Server/Server/main.c
+++ Server/Server/main.c
@@ -11,9 +11,37 @@
 #include "open_listenfd.h"
 #include "rio.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXLINE 1000
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
+    
+    int clientfd, port;
+    char *host, buf[MAXLINE];
+    rio_t rio;
+    
+    if(argc!=3){
+        fprintf(stderr,"usage: %s <host> <port> \n",argv[0]);
+        exit(0);
+    }
+    
+    host = argv[1];
+    port = atoi(argv[2]);
+    
+    clientfd = open_clientfd(host, port);
+    rio_readinitb(&rio, clientfd);
+    
+    while(fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        rio_writen(clientfd, buf, strlen(buf));
+        rio_readlineb(&rio, buf, MAXLINE);
+        fputs(buf, stdout);
+    }
+    close(clientfd);
+    exit(0);
+    
+    /* readinitb, rio_readlineb and riowriten test
     // insert code here...
     printf("Hello, World!\n");
 
@@ -25,5 +53,7 @@ int main(int argc, const char * argv[]) {
     while ((n = (int)rio_readlineb(&rio,buf,MAXLINE))) {
         rio_writen(STDOUT_FILENO,buf,n);
     }
+     
+     */
     return 0;
 }
