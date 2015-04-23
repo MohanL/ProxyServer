d93d60825db6fa4cd5ae03a0d3e24f04b58942f1
Mohan Liu <mohan.liu.personal@gmail.com>
2015-04-23 17:09:24 -0400

    stest.sh

--- Server/Server/stest.sh
+++ Server/Server/stest.sh
@@ -6,5 +6,5 @@
 #  Created by Mohan Liu on 4/23/15.
 #  Copyright (c) 2015 Mohan Liu. All rights reserved.
 
-./ProxyServer 8888
-./telnet 127.0.0.1 8888
+./ProxyServer 8889
+./telnet 127.0.0.1 8889
