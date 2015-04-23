b144e8ae4054d72afe4d71ebe891b2c05c204fea
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-23 15:32:47 -0400

    iterative echo server

--- Server/Server/iecho.c
+++ Server/Server/iecho.c
@@ -27,38 +27,30 @@
  */
 int main(int argc, char * argv[]) {
     
-    int listenfd,connfd, port,clientlen;
+    int listenfd, connfd, port;
+    socklen_t clientlen;
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp;
-    
-    if(argc != 2)
-    {
-        fprintf(stderr,"usage: %s <port> \n",argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
     port = atoi(argv[1]);
     
-    printf("ckpt1\n");
     listenfd = open_listenfd(port);
-    
-    while(1){
+    while (1) {
         clientlen = sizeof(clientaddr);
-        connfd = accept(listenfd, (void *)&clientaddr, &clientlen);
-        printf("ckpt2\n");
+        connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
         
-        // determine the domina name and IP address of the client
-        hp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        printf("ckpt3\n");
-        
+        /* Determine the domain name and IP address of the client */
+        hp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+                           sizeof(clientaddr.sin_addr.s_addr), AF_INET);
         haddrp = inet_ntoa(clientaddr.sin_addr);
-        printf("server connected to %s (%s)\n",hp->h_name,haddrp);
-        printf("ckpt4\n");
+        printf("server connected to %s (%s)\n", hp->h_name, haddrp);
         
         echo2(connfd);
         close(connfd);
-        printf("ckpt5\n");
-        
     }
     exit(0);
 }
