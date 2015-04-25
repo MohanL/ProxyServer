bd67cad9247b8aca87565cd4779000de115f51f3
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-25 15:55:36 -0400

    test

--- Server/Server/doit.c
+++ Server/Server/doit.c
@@ -51,6 +51,7 @@ void doit(int fd)
     {
         p = strtok(uri, "/");
         server = strtok (NULL, "/");
+        char * realserver;strcpy(realserver, server);
         suburi = p;
         //printf("%s\n",p);
         while (p != NULL)
@@ -64,7 +65,7 @@ void doit(int fd)
         
         if(strcmp(suburi, "http:")== 0)
             strcpy(suburi, "home.html");
-        sprintf(nbuf,"2.GET /%s HTTP/1.1\r\nhost:%s\r\n\r\n",suburi,server);
+        sprintf(nbuf,"2.GET /%s HTTP/1.1\r\nhost:%s\r\n\r\n",suburi,realserver);
         printf("%s",nbuf);
 
     }
@@ -73,6 +74,7 @@ void doit(int fd)
         //printf("case 2: there is no http\n");
         p = strtok(uri, "/");
         server = p;
+        char * realserver;strcpy(realserver, server);
         suburi = p;
         //printf("%s\n",p);
         while (p != NULL)
@@ -88,7 +90,7 @@ void doit(int fd)
         if(strcmp(suburi, server) == 0)
             strcpy(suburi, "home.html");
         //combine the request into (method suburi version\r\n host: server\r\n"
-        sprintf(nbuf,"2.GET /%s HTTP/1.1\r\nhost:%s\r\n\r\n",suburi,server);
+        sprintf(nbuf,"2.GET /%s HTTP/1.1\r\nhost:%s\r\n\r\n",suburi,realserver);
         
         printf("%s",nbuf);
     }
