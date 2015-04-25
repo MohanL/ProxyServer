f1602e3a1550a96337f6bf811a3800a9176c8eb9
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-25 02:43:06 -0400

    test

--- Server/Server/interclient.h
+++ Server/Server/interclient.h
@@ -10,5 +10,5 @@
 #define __Server__interclient__
 
 #include <stdio.h>
-extern int interclient(char * hostname,int port, char request[]);
+extern int interclient(char * hostname,int port, char request[], int fd);
 #endif /* defined(__Server__interclient__) */
