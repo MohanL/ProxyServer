c8db40b6e5639e9067d879ace0d341e5165f316a
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-26 17:44:06 -0400

    test

--- Server/Server/interclient.c
+++ Server/Server/interclient.c
@@ -76,7 +76,7 @@ int interclient(char * hostname,int port, char request[],int fd)
     bzero(server_reply,MAXBUF);
     while(recv(sock,server_reply , MAXBUF , 0))
     {
-        puts(server_reply);
+        //puts(server_reply);
         write(fd, server_reply , sizeof(server_reply));
 
         if(strchr(server_reply, '\0')!=NULL)
