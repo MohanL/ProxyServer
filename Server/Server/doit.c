e7b6b5455bcbdfcca1844df8c1b5de110df97367
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-27 17:24:44 -0400

    test

--- Server/Server/doit.c
+++ Server/Server/doit.c
@@ -58,9 +58,9 @@ void doit(int fd)
             suburi = p;
         }
         if(strcmp(suburi, "http:")== 0)
-            sprintf(nbuf,"GET /home.html HTTP/1.1\r\nhost:%s\r\n\r\nc",server);
+            sprintf(nbuf,"GET /home.html HTTP/1.1\r\nhost: %s\r\n\r\n",server);
         else
-            sprintf(nbuf,"GET %s HTTP/1.1\r\nhost:%s\r\n\r\nc",a,server);
+            sprintf(nbuf,"GET %s HTTP/1.1\r\nhost: %s\r\n\r\n",a,server);
     }
     else// case the server doesn't have http : GET www.cycle1.csug.rochester.edu/home.html HTTP/1.1
     {
@@ -75,23 +75,17 @@ void doit(int fd)
             suburi = p;
         }
         if(strcmp(suburi, server) == 0)
-            sprintf(nbuf,"GET home.html HTTP/1.1\r\nhost:%sa\r\nb\r\nc",server);
+            sprintf(nbuf,"GET home.html HTTP/1.1\r\nhost: %sa\r\nb\r\n",server);
         else
-            sprintf(nbuf,"GET %s HTTP/1.1\r\nhost:%s\r\n\r\nc",a,server);
+            sprintf(nbuf,"GET %s HTTP/1.1\r\nhost: %s\r\n\r\n",a,server);
     }
-    printf("%s",nbuf);
-    /*
-    unsigned long len = strlen(nbuf)+6;
+
+    unsigned long len = strlen(nbuf);
     char request[len];
     bzero(request, len);
     strncpy(request, nbuf, len);
-    */
-    char request[50];
-    bzero(request, 50);
-    strncpy(request, nbuf, 50);
     printf("%s",request);
-    //puts("REMOTE SERVER OUTPUT");
-    //interclient(server,80,request,fd);
+    interclient(server,80,request,fd);
     
     
     // this line of code doesn't really do anything, does it ?
