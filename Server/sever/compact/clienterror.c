2c54149d64cc56513bb47f16a28a552a3686cc2e
Mohan Liu <mohan.liu.personal@gmailc.com>
2015-04-27 01:48:21 -0400

    test

---
+++ Server/sever/compact/clienterror.c
@@ -0,0 +1,40 @@
+//
+//  clienterror.c
+//  Server
+//
+//  Created by Mohan Liu on 4/23/15.
+//  Copyright (c) 2015 Mohan Liu. All rights reserved.
+//
+
+#include "clienterror.h"
+#include "rio.h"
+#include <string.h>
+/*
+ * clienterror - returns an error message to the client
+ */
+/* $begin clienterror */
+#define	MAXLINE	 8192  /* Max text line length */
+#define MAXBUF   8192  /* Max I/O buffer size */
+#define LISTENQ  1024  /* Second argument to listen() */
+void clienterror(int fd, char *cause, char *errnum,
+                 char *shortmsg, char *longmsg)
+{
+    char buf[MAXLINE], body[MAXBUF];
+    
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+    
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    rio_writen(fd, buf, strlen(buf));
+    rio_writen(fd, body, strlen(body));
+}
+/* $end clienterror */
