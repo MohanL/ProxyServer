ae645b890dee8a793bd04eb4bfe39f3004bf1b30
Mohan Liu <mohan.liu.personal@gmail.com>
2015-05-14 21:41:07 -0400

    modified concurrent.py

--- server/compact/concurrent.py
+++ server/compact/concurrent.py
@@ -22,47 +22,16 @@ proxy = urllib2.ProxyHandler({"http":"http://" + iphost + ":" + `port`})
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
 
-def test_fetch_concurrent(n):
-	# concurrent connections
-	
-	clientsock = [None] * n
-	for x in range(0, n):
-		try:
-			# creating a client socket
-			clientsock[x] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-			# connecting to the proxy server and leaving the connection open
-			clientsock[x].connect((iphost, port))
-			#clientsock[x].send('G')
-		except Exception, e:
-			print 'Concurrent Fetch ' + str(n) + ': FAILED ' +  str(e)
-			return None
-        # this verifies that all the connections are connected .
-        # print 'all connections contructed'
-        # the problem is that it could not set up >=2 connections with the server.
-	    # This test verifies that your proxy server can handle concurrent (= 2) connections successfully.
-        try:
-            html = urllib2.urlopen("http://cs.rochester.edu/u/hedayati/csc252/lorem.html").read()
-            print ('Concurrent Fetch ' + str(n) + ': ') + 'PASSED' if hashlib.md5(html).hexdigest() == '5ac3495fa2ffab9e97d519ce8cff1b5c' else 'FAILED invalid hash:' + hashlib.md5(html).hexdigest()
-        except Exception, e:
-            print 'Concurrent Fetch ' + str(n) + ': FAILED ' +  str(e)
-
 def test_fetch_concurrent2():
     clientsock = [None] * 2
     try:
         clientsock[0] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         clientsock[1] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientsock[x].connect((iphost, port))
-        clientsock[x].connect((iphost, port))
+        clientsock[0].connect((iphost, port))
+        clientsock[1].connect((iphost, port))
     except Exception, e:
         print 'Concurrent Fetch 2'+ ': FAILED ' +  str(e)
         return None
-
-    try:
-        html = urllib2.urlopen("http://cs.rochester.edu/u/hedayati/csc252/lorem.html").read()
-        print ('Concurrent Fetch 2'+ ': ') + 'PASSED' if hashlib.md5(html).hexdigest() == '5ac3495fa2ffab9e97d519ce8cff1b5c' else 'FAILED invalid hash:' + hashlib.md5(html).hexdigest()
-    except Exception, e:
-        print 'Concurrent Fetch 2 ' + ': FAILED ' +  str(e)
 
 if __name__ == '__main__':
 	test_fetch_concurrent2()
